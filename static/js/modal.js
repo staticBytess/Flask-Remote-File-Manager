61ec5e94c58b6d8d70ea54cb8e0b9c4522f69ec3
staticBytess <160567397+staticBytess@users.noreply.github.com>
2025-11-16 16:00:20 -0800

    new 'process' button added for pop menu

---
+++ static/js/modal.js
@@ -0,0 +1,31 @@
+// Wait for the HTML document to be fully loaded before running any script
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("process");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    // This will only be added *after* the button exists
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+}); // <-- Make sure to close the new listener here
